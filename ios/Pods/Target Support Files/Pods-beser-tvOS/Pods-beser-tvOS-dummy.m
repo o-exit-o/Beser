8132671c4ae7157982e43b215faf662258ad1648
ag33953 <ghiberty.meza@anthem.com>
2019-12-11 18:01:39 -0500

    updated code

---
+++ ios/Pods/Target Support Files/Pods-beser-tvOS/Pods-beser-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_beser_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_beser_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-beser-tvOSTests/Pods-beser-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_beser_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_beser_tvOSTests
+@end
